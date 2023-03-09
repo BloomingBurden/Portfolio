504860b45a2ca6259ec47d3034a046b7f2558176
Blooming Burden <flowersoverskulls96@gmail.com>
2023-03-09 18:22:33 +0300

    Start project - start header

---
+++ src/js/modules/functions.js
@@ -0,0 +1,30 @@
+const isWebp = () => {
+    function testWebP(callback) {
+        let webP = new Image();
+        webP.onload = webP.onerror = function () {
+            callback(webP.height == 2);
+        };
+        webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    }
+
+    testWebP(function (support) {
+        let className = support ? 'webp' : 'no-webp';
+        document.documentElement.classList.add(className);
+    });
+};
+
+const debounce = (func, ms) => {
+    let timer = false;
+
+    return function (...arg) {
+        if (timer === true) {
+            return;
+        } 
+        timer = true;
+        func(arg);
+        
+        setTimeout(() => timer = false, ms);
+    }
+}
+
+isWebp();
