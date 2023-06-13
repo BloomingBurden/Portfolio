db3c351de6e7e5261b7a72a3ead1db183e83f207
Blooming Burden <flowersoverskulls96@gmail.com>
2023-06-13 18:11:19 +0300

    Min change

--- src/js/modules/openPopup.js
+++ src/js/modules/openPopup.js
@@ -79,11 +79,11 @@ const closePopup = (element) => {
     document.body.removeAttribute('style');
     document.body.classList.remove('stop-scrolling');
     mainWrapper.style.transform = 'translateX(0%)';
+    indexMain.removeAttribute('style');
    
     setTimeout(() => {
         element.remove();
-        indexMain.removeAttribute('style');
-    }, 1600);
+    }, 1000);
 };
 
 const renderPopup = (section) => {
